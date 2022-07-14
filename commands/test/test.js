ad114cba0937b650d072f189af2180b286159d7d
Arrow-07 <nonnoluigi46@gmail.com>
2022-07-14 17:12:29 +0200

    +canvas

--- commands/test/test.js
+++ commands/test/test.js
@@ -6,8 +6,21 @@ module.exports = {
     description: "comando di test",
     aliases: ["prova", "ping"],
     onlyStaff: false,
-    execute(message, args){     
+    execute(message, args, member){     
         
+            const {createCanvas, loadImage} = require("canvas")
+            let canvas = await createCanvas(1700, 600)
+            let ctx = await canvas.getContext("2d")
+    
+            let img = await loadImage("./img/background.png")
+            ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2)
+    
+            let channel = client.channels.cache.get("996715018957627444");
+       
+            let attachment = new Discord.MessageAttachment(canvas.toBuffer(), "canvas.png")
+            
+            channel.send({files: [attachment]})
+           
     
     }
 }
