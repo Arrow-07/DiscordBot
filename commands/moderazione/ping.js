4f23d5dc23b9b159c5ce8b7dc070e38ff9c2028f
Arrow-07 <nonnoluigi46@gmail.com>
2022-07-20 20:40:58 +0200

    beta 1.00

--- commands/moderazione/ping.js
+++ commands/moderazione/ping.js
@@ -5,34 +5,33 @@ module.exports = {
     aliases: ["test"],
     onlyStaff: false,
     execute(message, args){
-        var ping = client.ws.ping
-       if(ping > 119){
-        var embed = new Discord.MessageEmbed()
-            .setTitle("Ping del bot")
-            .setDescription("Ecco la latenza del bot")
-            .addField("Ping", `${client.ws.ping}ms`)
-            .setColor('RED') 
-            message.channel.send({embeds: [embed]})
-       }
-       else if(ping > 59 && ping < 120){
-        var embed1 = new Discord.MessageEmbed()
-            .setTitle("Ping del bot")
-            .setDescription("Ecco la latenza del bot")
-            .addField("Ping", `${client.ws.ping}ms`)
-            .setColor('ORANGE') 
-            message.channel.send({embeds: [embed1]})
-       }
-       else if(ping < 60){
-        var embed2 = new Discord.MessageEmbed()
-            .setTitle("Ping del bot")
-            .setDescription("Ecco la latenza del bot")
-            .addField("Ping", `${client.ws.ping}ms`)
-            .setColor('GREEN') 
-            message.channel.send({embeds: [embed2]})
-       }
         
-
-       
+            var ping = client.ws.ping
+           if(ping > 119){
+            var embed = new Discord.MessageEmbed()
+                .setTitle("Ping del bot")
+                .setDescription("Ecco la latenza del bot")
+                .addField("Ping", '${client.ws.ping}ms')
+                .setColor('RED') 
+                message.channel.send({embeds: [embed]})
+           }
+           else if(ping > 59 && ping < 120){
+            var embed1 = new Discord.MessageEmbed()
+                .setTitle("Ping del bot")
+                .setDescription("Ecco la latenza del bot")
+                .addField("Ping", '${client.ws.ping}ms')
+                .setColor('ORANGE') 
+                message.channel.send({embeds: [embed1]})
+           }
+           else{
+            var embed2 = new Discord.MessageEmbed()
+                .setTitle("Ping del bot")
+                .setDescription("Ecco la latenza del bot")
+                .addField("Ping", '${client.ws.ping}ms')
+                .setColor('GREEN') 
+                message.channel.send({embeds: [embed2]})
+           }
+    
         
     }
 }
---
+++ commands/moderazione/warn.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: "warn",
+    description: "warn",
+    aliases: [],
+    onlyStaff: false,
+    execute(message, args){
+        let idRuoloWarn1 = '999379652932018256'
+        let idRuoloWarn2 = '999382552190144552'
+        let utente = message.mentions.members.first();
+    if (!message.member.permissions.has('959156448422662174')) {
+        return message.channel.send('Non hai il permesso');
+    }
+    if (!utente) {
+        return message.channel.send('Non hai menzionato nessun utente');
+    }
+    let reason = message.content.split(/ +/).slice(2).join(" ");
+    if (!reason) {
+        return message.channel.send('Non hai specificato la motivazione del warn');
+    }
+
+    if (utente.roles.cache.has(idRuoloWarn1)) {
+        utente.roles.remove(idRuoloWarn1);
+        utente.roles.add(idRuoloWarn2);
+
+        let embed = new Discord.MessageEmbed()
+            .setTitle(`${utente.user.username} warnato`)
+            .setColor('RED')
+            .setDescription(`Utente warnato da ${message.author.toString()} per "**${reason}**"\nUn altro warn e l'utente verrà bannato`)
+        message.channel.send({ embeds: [embed] })
+    }
+    else if (utente.roles.cache.has(idRuoloWarn2)) {
+        utente.roles.remove(idRuoloWarn2);
+        utente.ban()
+
+        let embed = new Discord.MessageEmbed()
+            .setTitle(`${utente.user.username} warnato`)
+            .setColor('DARK_RED') 
+            .setDescription(`Utente warnato da ${message.author.toString()} per "**${reason}**"\nL'utente è stato bannato poichè ha raggiunto i 3 warn`)
+        message.channel.send({ embeds: [embed] })
+    }
+    else {
+        utente.roles.add(idRuoloWarn1);
+
+        let embed = new Discord.MessageEmbed()
+            .setTitle(`${utente.user.username} warnato`)
+            .setColor('ORANGE') 
+            .setDescription(`Utente warnato da ${message.author.toString()} per "**${reason}**"\nAltri 2 warn e l'utente verrà bannato`)
+        message.channel.send({ embeds: [embed] })
+    }
+}
+}
