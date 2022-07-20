586eb0838944a279699c790646630dd75e2d9142
Arrow-07 <nonnoluigi46@gmail.com>
2022-07-20 18:53:05 +0200

    Update ping.js

--- commands/moderazione/ping.js
+++ commands/moderazione/ping.js
@@ -6,20 +6,29 @@ module.exports = {
     onlyStaff: false,
     execute(message, args){
         var ping = client.ws.ping
-       if(ping > 10){
+       if(ping > 119){
         var embed = new Discord.MessageEmbed()
             .setTitle("Ping del bot")
             .setDescription("Ecco la latenza del bot")
             .addField("Ping", `${client.ws.ping}ms`)
             .setColor('RED') 
             message.channel.send({embeds: [embed]})
-       }else{
+       }
+       else if(60 > ping < 120 ){
         var embed1 = new Discord.MessageEmbed()
             .setTitle("Ping del bot")
             .setDescription("Ecco la latenza del bot")
             .addField("Ping", `${client.ws.ping}ms`)
-            .setColor('GREEN') 
+            .setColor('ORANGE') 
             message.channel.send({embeds: [embed1]})
+       }
+       else if(ping < 60){
+        var embed2 = new Discord.MessageEmbed()
+            .setTitle("Ping del bot")
+            .setDescription("Ecco la latenza del bot")
+            .addField("Ping", `${client.ws.ping}ms`)
+            .setColor('GREEN') 
+            message.channel.send({embeds: [embed2]})
        }
         
 
